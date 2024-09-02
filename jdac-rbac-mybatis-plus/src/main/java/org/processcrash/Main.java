1131245baa814af6a52a6fdd4d6cbe78181b4c8b
张晓强 <zhangxq@loroi.com>
2024-09-02 11:37:55 +0800

    项目初始化

---
+++ jdac-rbac-mybatis-plus/src/main/java/org/processcrash/Main.java
@@ -0,0 +1,11 @@
+package org.processcrash;
+
+/**
+ * @author zhangxq
+ * @since 2024/9/2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
